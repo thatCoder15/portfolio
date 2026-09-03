10d75709ff851e6ed0b67aa22f51bc185940b3aa
i-manoj <curiousmanoj4@gmail.com>
2026-09-04 03:43:06 +0530

    remobed download

--- src/app/components/ScrollToTop/index.tsx
+++ src/app/components/ScrollToTop/index.tsx
@@ -34,14 +34,14 @@ export default function ScrollToTop() {
       {isVisible && (
         <div className='fixed bottom-8 right-8 z-999'>
           <div className='flex items-center gap-2.5'>
-            <Link
+            {/* <Link
               href={
                 'https://getnextjstemplates.com/products/desgy-nextjs-free-landing-page-template'
               }
               target='_blank'
               className='hidden lg:block bg-primary text-white hover:bg-darkmode text-sm px-4 py-3.5 leading-none rounded-lg font-medium text-nowrap'>
               Download Now
-            </Link>
+            </Link> */}
             {isVisible && (
               <div
                 onClick={scrollToTop}
